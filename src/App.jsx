6dbe995c62b676245141c4ae3dd9576c74f3185f
hbandi93 <hbandi93@gmail.com>
2022-01-11 19:14:38 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { useState } from "react"
 const App = () => {
   let [isLandingPage, setterF] = useState(true);
 
+  let [pageName, setPageName] = useState("Home");
+
   return (
     <div>
-      <p>Hello</p>
+      <p>Hülye react</p>
       <button onClick={() => setterF(!isLandingPage)}>Click me!</button>
       {isLandingPage ? <div>Landing page</div> : <div>Homepage</div>}
       <div>
-
+        <hr />
+        <button onClick={() => setPageName("Home")}>Home</button>
+        <button onClick={() => setPageName("Landing")}>Landing</button>
+        <button onClick={() => setPageName("About")}>About</button>
+        {pageName === "Home" && <div>Text</div>}
+        {pageName === "About" && <div>Text2</div>}
+        {pageName === "Landing" && <div>Text3</div>}
       </div>
     </div>
   );
