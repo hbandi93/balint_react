730e0313af55362ed497a89e1cee9944dbc108ad
hbandi93 <hbandi93@gmail.com>
2022-01-11 11:39:54 +0100

    Meh

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p className="par">ASD</p>
+      <div id="egyedi"></div>
+      <input type="text" placeholder="password" />
+    </div>
+  );
+}
+
+export default App;
