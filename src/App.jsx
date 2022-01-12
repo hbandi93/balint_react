596d06c392d1cabe122bee6079ffe96f4931eb98
hbandi93 <hbandi93@gmail.com>
2022-01-12 13:35:27 +0100

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react"
+let inputValue = "Ez az input értéke"
 
 const App = () => {
-  let [isLandingPage, setterF] = useState(true);
-
-  let [pageName, setPageName] = useState("Home");
-
   return (
     <div>
-      <p>Hülye react</p>
-      <button onClick={() => setterF(!isLandingPage)}>Click me!</button>
-      {isLandingPage ? <div>Landing page</div> : <div>Homepage</div>}
-      <div>
-        <hr />
-        <button onClick={() => setPageName("Home")}>Home</button>
-        <button onClick={() => setPageName("Landing")}>Landing</button>
-        <button onClick={() => setPageName("About")}>About</button>
-        {pageName === "Home" && <div>Text</div>}
-        {pageName === "About" && <div>Text2</div>}
-        {pageName === "Landing" && <div>Text3</div>}
-      </div>
+      <input type="text" value={inputValue} />
     </div>
   );
 }
